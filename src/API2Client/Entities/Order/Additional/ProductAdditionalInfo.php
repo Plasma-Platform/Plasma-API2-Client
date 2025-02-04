911040e81c3ea5eabdb771ae55a76c7df43ba115
novoseltsev <dima.nowoseltsew@gmail.com>
2025-02-04 15:31:37 +0200

    TMONE-894 - add product additional info

---
+++ src/API2Client/Entities/Order/Additional/ProductAdditionalInfo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace API2Client\Entities\Order\Additional;
+
+class ProductAdditionalInfo implements AdditionalInfoInterface
+{
+    /**
+     * @var string
+     */
+    protected $orderId;
+
+
+    /**
+     * @param string $orderId
+     */
+    public function setOrderId($orderId)
+    {
+        $this->orderId = $orderId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return array_filter(array(
+            'order_id' => $this->getOrderId(),
+        ));
+    }
+}
